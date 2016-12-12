b924d805d5b41711d27cc3ec3ebed03d285f1663
AlexeyZelenetskiy <Zelenetskiy_alexey@mail.ru>
2016-12-13 02:40:14 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,100 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QDebug>
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    k = 0;
+    connect(ui->actionOpen, SIGNAL(triggered()), SLOT(openClicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::openClicked()
+{
+    Dialog *dlg = new Dialog(this);
+    dlg->show();
+
+    connect(dlg, SIGNAL(pathSelected(QString)), SLOT(getPath(QString)));
+
+}
+
+void MainWindow::getPath(QString path)
+{
+    try{
+        QFile file(path);
+        if(!file.open(QIODevice::ReadOnly))
+            return;
+
+        QString xml = QString::fromUtf8(file.readAll());
+
+        if(xml[1] == '?')
+           xml = xml.mid(xml.indexOf("?>") + 2, xml.size() - xml.indexOf("?>") - 2);
+
+
+        while(xml.indexOf('\r') != -1 || xml.indexOf('\n') != -1)
+        {
+            int i = xml.indexOf('\r');
+            xml.remove(i, 1);
+
+            i = xml.indexOf('\n');
+            xml.remove(i, 1);
+
+        }
+
+
+        tree obj(xml);
+        QString body = obj.body__();
+        const int lim = 2500;
+        while(body.indexOf("</p>") > -1)
+        {
+            QString str;
+            int h = 0;
+            while(h != 15)
+            {
+                int j = body.indexOf("</p>");
+                if(str.size() > lim)
+                    break;
+                str += body.mid(0, j + 4);
+                body = body.mid(j + 4, body.size() - j -4);
+                h++;
+            }
+            vec.push_back(str);
+        }
+
+        ui->textBrowser->setHtml(vec[0]);
+        QString lbl = QString::number((k+1)) + '/' + QString::number((vec.size() + 1));
+        ui->label->setText(lbl);
+        file.close();
+    }
+    catch(const std::exception & ex){
+        qDebug() << QString(ex.what());
+    }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(k != vec.size() - 1)
+      ui->textBrowser->setHtml(vec[++k]);
+   QString lbl = QString::number((k+1)) + '/' + QString::number((vec.size() + 1));
+    ui->label->setText(lbl);
+}
+
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    if(k != 0)
+      ui->textBrowser->setHtml(vec[--k]);
+    QString lbl = QString::number((k+1)) + '/' + QString::number((vec.size() + 1));
+    ui->label->setText(lbl);
+}
---
+++ tree.h
@@ -0,0 +1,52 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <locale>
+#include <QDebug>
+#include <QString>
+#include <QDebug>
+#include <memory>
+
+using namespace std;
+
+class tree
+{
+    struct Node {
+        QString name;
+        QString data;
+        QString attribute;
+        shared_ptr<Node> son;
+        shared_ptr<Node> brother;
+
+        Node() {}
+
+        Node(QString name, QString data, QString attribute, shared_ptr<Node> son = shared_ptr<Node>(nullptr), shared_ptr<Node> brother = shared_ptr<Node>(nullptr))
+            : name(name), data(data), son(son), brother(brother), attribute(attribute) {}
+
+        Node(Node & node)
+            : name(node.name), data(node.data), attribute(node.attribute), son(node.son), brother(node.brother) {}
+    };
+
+     shared_ptr<Node> root;
+public:
+    tree() {}
+    tree(QString & xml)
+        : root(new Node(firstElement(xml).name, "", firstElement(xml).attribute))
+    {
+        _xml = xml;
+        pars(crop(xml, *root), *root);
+    }
+
+    shared_ptr<Node> getById(QString _name, shared_ptr<Node> _root = shared_ptr<Node>(nullptr));
+    QString body__();
+private:
+    QString _xml;
+    void pars(QString & xml, Node & node);
+    Node firstElement(const QString & xml);
+    QString crop(QString & xml, Node & item);
+};
+
+#endif // TREE_H
